4aadc438d9993235397bf55b11ee90c4e6a22872
Holger Brandl <brandl@mpi-cbg.de>
2014-07-18 12:50:17 +0200

    fixed expression filter

--- R/bio/cummerutils.R
+++ R/bio/cummerutils.R
@@ -1,14 +1,14 @@
 
-
 require.auto(cummeRbund)
 
 
+
 getExpressedGenes <- function(cuff, minFPKM=1, logMode=F){
-    fpkmMat<-repFpkmMatrix(genes(cuff))
+    fpkmMat<-repFpkmMatrix(genes(mmusCuff))
 
     if(logMode) fpkmMat<-log10(fpkmMat+1) ## add a pseudocount
 
     geneMax <- apply(fpkmMat, 1, max)
 
-    fpkmMat[geneMax>minFPKM,]$gene_id
+    rownames(fpkmMat[geneMax>minFPKM,])
 }
