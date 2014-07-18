06ff2398b793694582f9d5a9119480fab4110930
Holger Brandl <brandl@mpi-cbg.de>
2014-07-18 12:39:06 +0200

    started new utils package on github

---
+++ R/bio/cummerutils.R
@@ -0,0 +1,14 @@
+
+
+require.auto(cummeRbund)
+
+
+getExpressedGenes <- function(cuff, minFPKM=1, logMode=F){
+    fpkmMat<-repFpkmMatrix(genes(cuff))
+
+    if(logMode) fpkmMat<-log10(fpkmMat+1) ## add a pseudocount
+
+    geneMax <- apply(fpkmMat, 1, max)
+
+    fpkmMat[geneMax>minFPKM,]$gene_id
+}
