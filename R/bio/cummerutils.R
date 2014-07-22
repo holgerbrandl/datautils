f4cf635eec30c7bcadb40d176a05c240266df81a
Holger Brandl <brandl@mpi-cbg.de>
2014-07-22 16:45:00 +0200

    cont. cummerbund methods

--- R/bio/cummerutils.R
+++ R/bio/cummerutils.R
@@ -1,14 +1,18 @@
 
-require.auto(cummeRbund)
+require(cummeRbund)
 
 
-
-getExpressedGenes <- function(cuff, minFPKM=1, logMode=F){
+getExpressedGenes <- function(cuff, ...){
     fpkmMat<-repFpkmMatrix(genes(cuff))
 
+    rownames(filterByExpression(fpkmMat, ...))
+}
+
+
+filterByExpression <- function(fpkmMat, minFPKM=1, logMode=F){
     if(logMode) fpkmMat<-log10(fpkmMat+1) ## add a pseudocount
 
     geneMax <- apply(fpkmMat, 1, max)
 
-    rownames(fpkmMat[geneMax>minFPKM,])
+    fpkmMat[geneMax>minFPKM,]
 }
