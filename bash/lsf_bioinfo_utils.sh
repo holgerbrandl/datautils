8cc384782592ead036f3bd22860ab730b55be706
Holger Brandl <brandl@mpi-cbg.de>
2014-08-18 13:26:45 +0200

    cosmetics

--- bash/lsf_bioinfo_utils.sh
+++ bash/lsf_bioinfo_utils.sh
@@ -2,18 +2,17 @@
 ## create fastq report for all fastq and fastq.gz files in the current directory
 mmFastqc(){
     outputDir="fastqc_reports"
-#    filePattern="fastq.gz"
-#    outputDir=$1
-#    filePattern=$2
+
     mkdir $outputDir
+
     for fastqFile in *fastq *fastq.gz ; do
         if [ ! -f $fastqFile ]; then
             continue;
         fi
-        echo processing $fastqFile
-#        bsub -q medium --J fastqc_$(basename $fastqFile) "fastqc -j /sw/bin/java -o $outputDir -f fastq $fastqFile" | joblist .fastqc_jobs
+
         mysub "fastqc__$(basename $fastqFile)" "fastqc -j /sw/bin/java -o $outputDir -f fastq $fastqFile" -q medium  | joblist .fastqc_jobs
     done
+
     wait4jobs .fastqc_jobs
 }
 export -f mmFastqc
