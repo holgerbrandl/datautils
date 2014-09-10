d57cc6873d087bef09903d314bc36014605e226a
Holger Brandl <brandl@mpi-cbg.de>
2014-09-10 23:19:00 +0200

    cont.

--- R/rknit/rknit_preprocessor.R
+++ R/rknit/rknit_preprocessor.R
@@ -9,8 +9,8 @@ mdRegex <- "^#([#]*)> ([^{]*)([{]+(.+)[}])?"
 #str_match("##> test heading {fig.width=10}", mdRegex)
 #str_match("#> simple markup", mdRegex)
 #str_match("#>  {fig.width=10}", mdRegex)
-#str_match("#> * normBxx >0 --> horizontal", mdRegex)
+#str_replace("#> * normBxx >0 --> horizontal", mdRegex, "```\n\\1\\2\n```{r \\4}") %>% paste(collapse="\n") %>% cat()
 
-#readLines(file("/Users/brandl/Dropbox/Public/datautils/R/rknit/rknit_example.R")) %>%
-readLines(file("stdin")) %>%
+readLines(file("/Users/brandl/Dropbox/Public/datautils/R/rknit/rknit_example.R")) %>%
+#readLines(file("stdin")) %>%
     str_replace(mdRegex, "```\n\\1\\2\n```{r \\4}") %>% paste(collapse="\n") %>% cat()
