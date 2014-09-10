c12fbb6e1caebf50269478c650ad8f2fd259e206
Holger Brandl <brandl@mpi-cbg.de>
2014-09-10 17:29:34 +0200

    cont. reverse knitr

--- bash/rknit.sh
+++ bash/rknit.sh
@@ -2,13 +2,18 @@
 
 ## just knit R documents as they are, no markdown is required (but supported to some extent)
 rknit(){
+    ## rscript=/projects/project-raphael/Rcode/misc/DivisionPerpendicularity.R
+    ## rscript=/projects/project-raphael/Rcode/misc/DivisionPerpendicularity.R
     rscript=$1
-    tmdRmd=$(mktemp  --suff .Rmd)
+#    tmdRmd=$(mktemp  --suff .Rmd)
+    tmdRmd=$(basename $rscript .R).Rmd
 
-    echo '# '$(basename $rscript .R) >> $tmdRmd
+    echo '# '$(basename $rscript .R) > $tmdRmd
     echo '```{r} ' >> $tmdRmd
-    cat $rscript >> $tmdRmd
+    cat $rscript | Rscript  -e 'require(stringr); require(dplyr); readLines(file("stdin")) %>% str_replace("##> (.*)", "```\n\\1\n```{r}") %>% writeLines(stdout()) ' >> $tmdRmd
     echo '```' >> $tmdRmd
 
-    echo "require(knitr); knit2html('$tmdRmd')" | R --vanilla -q
+    echo 'require(knitr); opts_knit$set(cache = TRUE); knit2html("'$tmdRmd'", output="'$(basename $rscript .R)'")' | R --vanilla -q
 }
+
+
