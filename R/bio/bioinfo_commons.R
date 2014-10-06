5210187ca9afcd9ce284225217ca4b80624d0da1
Holger Brandl <brandl@mpi-cbg.de>
2014-10-06 15:28:32 +0200

    converted into spinable doc; migrated to dplyr

--- R/bio/bioinfo_commons.R
+++ R/bio/bioinfo_commons.R
@@ -32,20 +32,21 @@ getSeqLengthFromFasta <- function(fileName){
 
 
 ## necessary to disable scientific number formats for long integers
-options(scipen=100)
+#options(scipen=100)
 
 ## writes a table in bed format expecting columns being ordered according to bed spec already
-write.bed <- function(bedData, file){
-    write.table(bedData, file=file, quote=FALSE, sep ="\t", na="NA", row.names=FALSE, col.names=FALSE)
-}
-
 #write.bed <- function(bedData, file){
-#    oldScipen<-getOption("scipen")
-#
-#     ## necessary to disable scientific number formats for long integers
-#    options(scipen=100)
-#
 #    write.table(bedData, file=file, quote=FALSE, sep ="\t", na="NA", row.names=FALSE, col.names=FALSE)
-#
-#    options(oldScipen)
 #}
+
+write.bed <- function(bedData, file){
+    oldScipen<-getOption("scipen")
+
+     ## necessary to disable scientific number formats for long integers
+    options(scipen=100)
+
+    write.table(bedData, file=file, quote=FALSE, sep ="\t", na="NA", row.names=FALSE, col.names=FALSE)
+
+    ## restore old scipen value
+    options(scipen=oldScipen)
+}
