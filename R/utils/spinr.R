410c66956ca6f8cd8a2dee3c44cf87d4305153fa
Holger Brandl <brandl@mpi-cbg.de>
2014-11-26 09:54:02 +0100

    cont. rna-seq workflow

--- R/utils/spinr.R
+++ R/utils/spinr.R
@@ -11,10 +11,10 @@ options(width=150)
 ## better table css: http://www.stat.ubc.ca/~jenny/STAT545A/topic10_tablesCSS.html
 spin(rScript, knit=F)
 
-mdScript <- str_replace(rScript, "[.]R$", ".Rmd")
+rmdScript <- str_replace(rScript, "[.]R$", ".Rmd")
 
-system(paste("mv", mdScript, "tmp.Rmd"))
-system(paste("cat tmp.Rmd | grep -Ev '^#+$' | grep -Fv '#!/usr/bin/env Rscript' >", basename(mdScript)))
+system(paste("mv", rmdScript, "tmp.Rmd"))
+system(paste("cat tmp.Rmd | grep -Ev '^#+$' | grep -Fv '#!/usr/bin/env Rscript' >", basename(rmdScript)))
 file.remove("tmp.Rmd")
 
 cssHeader='
@@ -27,6 +27,8 @@ cssHeader='
 
 ## custom title http://stackoverflow.com/questions/14124022/setting-html-meta-elements-with-knitr
 opts_chunk$set(cache = TRUE, fig.width=10, width=120)
-knit2html(basename(mdScript), header=cssHeader)
+knit2html(basename(rmdScript), header=cssHeader)
 
-file.remove(basename(mdScript))
+file.remove(basename(rmdScript))
+file.remove(basename(str_replace(rScript, "[.]R$", ".md")
+))
