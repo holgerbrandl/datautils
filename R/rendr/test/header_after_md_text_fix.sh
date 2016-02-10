26ecfa803c69935c6c24a919efe4ea4449442642
Holger Brandl <holgerbrandl@gmail.com>
2016-02-10 13:04:56 +0100

    started genome correction

--- R/rendr/test/header_after_md_text_fix.sh
+++ R/rendr/test/header_after_md_text_fix.sh
@@ -1,5 +1,6 @@
 
 cat /Users/brandl/Dropbox/Public/datautils/R/rendr/test/simple_report.R | sed 's/#\x27 #/#\x27\'$'\n#\x27 #/g'
+cat tt.R | sed 's/#\x27 #/#\x27\'$'\n#\x27 #/g'
 
 ## this works but the \ needs to be escaped in the R command as \\
 cat /Users/brandl/Dropbox/Public/datautils/R/rendr/test/simple_report.R |  sed 's/_TODAY_/'$(date +\"%m-%d-%Y\")'/g' | sed 's/#\x27 #/#\x27\'$'\n#\x27 #/g' | grep -Fv '#!/usr/bin/env Rscript'
