08729b53a0b3878a64faea2da02198ed8dea51b7
Holger Brandl <holgerbrandl@gmail.com>
2016-06-24 14:42:55 +0200

    added method to detach all (except the base) packages

--- R/core_commons.R
+++ R/core_commons.R
@@ -296,6 +296,13 @@ reload_dplyr <- function(){
 }
 
 
+## from http://stackoverflow.com/questions/7505547/detach-all-packages-while-working-in-r
+detachAllPackages <- function() {
+    basic.packages <- c("package:stats","package:graphics","package:grDevices","package:utils","package:datasets","package:methods","package:base")
+    package.list <- search()[ifelse(unlist(gregexpr("package:",search()))==1,TRUE,FALSE)]
+    package.list <- setdiff(package.list,basic.packages)
+    if (length(package.list)>0)  for (package in package.list) detach(package, character.only=TRUE)
+}
 
 
 ## workaround for biomart
