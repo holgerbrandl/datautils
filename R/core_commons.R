d3e415b7df44bbfb043a702e695660c6a98864ec
Holger Brandl <holger.brandl@systema.com>
2020-02-24 12:36:02 +0100

    added pointer to validate

--- R/core_commons.R
+++ R/core_commons.R
@@ -675,6 +675,7 @@ getenv_or_fail = function(name){
 
 
 # https://stackoverflow.com/questions/53157410/function-in-r-for-validating-existence-of-specific-columns-on-a-data-frame
+# for in-depth validation of column see http://www.markvanderloo.eu/yaRb/2016/03/25/easy-data-validation-with-the-validate-package/
 assert_columns <- function(df, columns){
     if (! is.data.frame(df)) stop(paste("Argument", deparse(substitute(df)), "must be a data.frame."))
 
